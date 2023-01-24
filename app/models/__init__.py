2fe90417d9ccd15f066c20d2e03f81b6db0a76f0
ascrivener@scrivener.systems <ascrivener@scrivener.systems>
2023-01-23 20:05:03 -0600

    includes additional model classes for tags and recipes.

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -8,22 +8,22 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(128), unique=True)
     pass_hash = db.Column(db.String(256), unique=True)
-    recipe_history = db.relationship()
-
-    def __init__(self, id, username, pass_hash, recipe_history):
-        self.id = id
-        self.username = username
-        self.pass_hash = pass_hash
-        self.recipe_history = recipe_history
+    recipe_history = db.relationship('Recipe', lazy='subquery', secondary=recipe_history, backref=db.backref('users', lazy=True) )
 
     def __repr__(self):
         return f'User account: {self.username}.'
 
-recipe_history = db.Table(
-    db.Column('user_id', db.Integer, db.ForeignKey('users.id'), primary_key=True)
+recipe_history = db.Table('tags',
+    db.Column('user_id', db.Integer, db.ForeignKey('users.id'), primary_key=True),
     db.Column('recipe_id', db.Integer, db.ForeignKey('recipes.id'), primary_key=True)
 )
 
+
+recipe_tags = db.Table(
+    'recipe_tags',
+    db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'), primary_key=True),
+    db.Column('recipe_id', db.Integer, db.ForeignKey('recipe.id'), primary_key=True)
+)
 
 class Recipe(db.Model):
     __tablename__ = 'recipes'
@@ -31,4 +31,26 @@ class Recipe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(512), nullable=False)
     description = db.Column(db.String(1024))
-    
+    tags = db.relationship('Tag', secondary=recipe_tags, lazy='subquery',
+    backref=db.backref('recipes', lazy=True)
+    )
+
+
+
+class Tag(db.Model):
+    __tablename__ = 'tags'
+
+    id = db.Column(db.Integer, primary_key=True)
+    tag_name = db.Column(db.String(64), unique=True)
+
+
+class Ingredient(db.Model):
+    __tablename__ = 'ingredients'
+
+    id = db.Column(db.Integer, primary_key=True)
+    details = db.Column(db.String(256))
+    recipe_id = db.Column(db.Integer, db.ForeignKey('recipes.id'))
+    recipes = db.relationship('Recipe', backref='ingredients', lazy='dynamic')
+
+
+
