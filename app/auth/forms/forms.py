d7ea3147d40a4472c4e7bc5f072e0fcf19c36d64
ascrivener@scrivener.systems <ascrivener@scrivener.systems>
2023-01-25 09:24:35 -0600

    adds add_recipe function

--- app/auth/forms/forms.py
+++ app/auth/forms/forms.py
@@ -1,13 +1,16 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SelectMultipleField
+from wtforms import StringField, PasswordField, BooleanField, SelectMultipleField, EmailField, SubmitField
 from wtforms.validators import Length, DataRequired, EqualTo
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
+    email = EmailField('Email', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired(), Length(12, 256), EqualTo('password2')])
     password2 = PasswordField('Confirm Password', validators=[DataRequired(), Length(12, 256)])
+    submit = SubmitField('Register')
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired('You must type your username')])
+    email = StringField('Email', validators=[DataRequired('You must type your username')])
     password = PasswordField('Password', validators=[DataRequired(), Length(12, 256)])
     stay_logged = BooleanField('Stay Logged in.', )
+    submit = SubmitField('Submit')
