80aca62238d2ea70a7fa81d086651889fa3c6d0e
ascrivener@scrivener.systems <ascrivener@scrivener.systems>
2023-02-06 21:48:32 -0600

    migrats static file for recipes to local static file

--- app/recipes/__init__.py
+++ app/recipes/__init__.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 
-blp = Blueprint('recipes', __name__, template_folder='templates/recipes',url_prefix='/recipes')
+blp = Blueprint('recipes', __name__, template_folder='templates/recipes',url_prefix='/recipes',static_folder='static/recipes')
 
 
 from app.recipes.views import views
