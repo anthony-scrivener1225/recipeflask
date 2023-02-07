c3f92a298f909ee52b7ecfa2b32eb072e3da947a
ascrivener@scrivener.systems <ascrivener@scrivener.systems>
2023-02-07 16:53:58 -0600

    View for specific Tag showing all recipes with tag relationship.
    Includes links of tags at bottom of recipe with url redirect to specific tag page.
    
    Adds Redirect variable so searching for a specific tag or adding to the tag url -> if it doesn't exist redirects to add tag page and prepopulates value with searched term

--- app/recipes/views/views.py
+++ app/recipes/views/views.py
@@ -2,7 +2,7 @@ from flask import session, render_template, url_for, redirect, request, flash
 from app.recipes import blp
 from flask_login import login_required
 from app.recipes.forms import AddRecipe, AddTag
-from app.models import Recipe, Tag, Ingredient, Direction
+from app.models import Recipe, Tag, Ingredient, Direction, recipe_tags
 from app import db
 from flask_login import current_user
 from random import choice
@@ -14,15 +14,17 @@ import os
 def add_recipe():
     form = AddRecipe()
     if form.is_submitted():
-        uploaded_file = form.file.data
-        if uploaded_file.filename != '':
-            sep_file = os.path.splitext(uploaded_file.filename)[1]
-        recipe = Recipe(name=form.recipe_name.data,description=form.recipe_description.data,created_by=current_user.id)
+        form_description = str(form.recipe_description.data)
+        recipe_desc = form_description if len(form_description) >= 64 else form_description+((64 - len(form_description)) * " ")
+        recipe = Recipe(name=form.recipe_name.data,description=recipe_desc,created_by=current_user.id)
         recipe.tags = form.recipe_tags.data
         db.session.add(recipe)
         db.session.commit()
-        recipe.image = str(recipe.id)+sep_file
-        uploaded_file.save(url_for('recipes.static', filename='recipes/'+recipe.image))
+        uploaded_file = form.file.data
+        if uploaded_file.filename != '':
+            sep_file = os.path.splitext(uploaded_file.filename)[1]
+            recipe.image = str(recipe.id)+sep_file
+            uploaded_file.save(os.path.join(basedir+'/app/recipes/static/recipes/'+recipe.image))
         for ingre in form.recipe_ingredients.data:
             new_ingredient = Ingredient(details=ingre['ingredient'],recipe_id=recipe.id)
             db.session.add(new_ingredient)
@@ -49,10 +51,21 @@ def delete_recipe(recipe_id=None):
         return render_template('myrecipes.html')
     
 
+@blp.route('/<tag>')
+def recipe_by_tag(tag=None):
+    tag_name = str(tag).capitalize()
+    recipe_tag = Tag.query.filter_by(name=tag_name).first()
+    if recipe_tag != None and recipe_tag != '':
+        tagged_recipes = Recipe.query.join(recipe_tags).join(Tag).filter(Tag.name == tag_name).all()
+        return render_template('recipebytag.html', tagged_recipes=tagged_recipes, tag_name=tag_name)
+    else:
+        return redirect(url_for('recipes.add_tag', search_tag=tag_name))
+
 
 @blp.route('/addtag', methods=["GET","POST"])
+@blp.route('/addtag/<search_tag>', methods=['GET','POST'])
 @login_required
-def add_tag():
+def add_tag(search_tag=None):
     form = AddTag()
     if form.validate_on_submit():
         tag = Tag(name=form.tag_name.data.replace(" ",""))
@@ -60,5 +73,5 @@ def add_tag():
         db.session.commit()
         form.tag_name.data = ""
         flash('Tag added successfully.',category='alert-success')
-    return render_template('addtag.html', form=form)
+    return render_template('addtag.html', form=form, search_tag=search_tag)
     
